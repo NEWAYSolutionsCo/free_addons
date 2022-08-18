4edf9e01afbceeea689eacb741f0fd1fc286e59c
NEWAYSolutionsCo <newaysolutions.co@gmail.com>
2022-08-18 17:54:09 +0100

    Upload our first free app

---
+++ hide_powered_by_odoo/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Hide Powered By Odoo',
+    'version': '15.0.1.0.0',
+    'sequence': 1,
+    'summary': 'Hide Powered By Odoo in login screen.',
+    'description': "Hide Powered By Odoo in login screen.",
+    'author': 'NEWAY Solutions',
+    'maintainer': 'NEWAY Solutions',
+    'price': '0.0',
+    'currency': 'USD',
+    'website': 'https://neway-solutions.com',
+    'license': 'LGPL-3',
+    'images': [
+        'static/description/icon.png'     
+    ],
+    'depends': [
+        'web'
+    ],
+    'data': [
+        'views/login_templates.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
