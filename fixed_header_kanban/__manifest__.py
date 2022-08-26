d3234bf41083e7ca62b3a7730de7c140dbeab06d
Ala MAAOUI <AMi@iptech.corp>
2022-08-26 16:03:17 +0100

    Update Free apps

--- fixed_header_kanban/__manifest__.py
+++ fixed_header_kanban/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Fixed Header Kanban View',
     'version': '13.0.1.0.0',
     'sequence': 1,
-    'summary': 'Sticky Header in Kanban View.',
+    'summary': 'Set Fix Header Kanban, Set Permanent Header Kanban, Web Sticky Header Kanban, Freeze Header Kanban, Set Fix Header, Set Permanent Header, Web Sticky Header, Freeze Header, Set Fix Header in Kanban, Set Permanent Header in Kanban, Web Sticky Header in Kanban, Freeze Header in Kanban, Set Fix Kanban Header, Set Permanent Kanban Header, Web Sticky Kanban Header, Freeze Kanban Header, All in one Fix Header, All in one Permanent Header, All in one Web Sticky Header, All in one Freeze Header',
     'description': "Fixed Header Kanban View is very useful for displaying kanban headers within Odoo.",
     'author': 'NEWAY Solutions',
     'maintainer': 'NEWAY Solutions',
@@ -14,7 +14,7 @@
     'website': 'https://neway-solutions.com',
     'license': 'LGPL-3',
     'images': [
-        'static/description/icon.png'    
+        'static/description/screenshot.gif'     
     ],
     'depends': [
         'web'
