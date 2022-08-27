087657517eef16225065ef469cffc121daae77af
NEWAYSolutionsCo <newaysolutions.co@gmail.com>
2022-08-27 19:40:41 +0100

    Update Free apps

--- show_hide_password/__manifest__.py
+++ show_hide_password/__manifest__.py
@@ -5,7 +5,10 @@
     'name': 'Show/Hide Password',
     'version': '13.0.1.0.0',
     'sequence': 1,
-    'summary': 'Show/Hide Password in login screen, Show Password Login Page, Hide Password Login Page, Web Responsive login, Odoo Web Login Page, Web backend login, Odoo login',
+    'summary': """
+        Show/Hide Password in login screen, View Password Login Page, Show Password Login Page, Hide Password Login Page, Web Responsive login, Odoo Web Login Page, Web backend login, Odoo login,
+        Show/Hide Password in sign in screen, View Password SignIn Page, Show Password SignUp Page, Hide Password SignIn Page, Web Responsive SignIn, Odoo Web SignIn Page, Web backend SignIn, Odoo SignUp Page, Odoo SignIn Page
+    """,
     'description': "You can show and hide your password in login screen.",
     'author': 'NEWAY Solutions',
     'maintainer': 'NEWAY Solutions',
