492f28664d7c3efd4950ebd819bdcb9fa297791a
NEWAYSolutionsCo <newaysolutions.co@gmail.com>
2022-09-20 11:58:06 +0100

    Add New App

---
+++ widget_many2many_tags/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Many2Many Tags Widget',
+    'version': '14.0.1.0.0',
+    'sequence': 1,
+    'summary': 'Colorful Widget, Colorful Many2Many Link Tags Widget, Web Widget Tags Many2Many, Tags Widget Link, Many2Many Link Tags, Design Widget, Design Input Widget, Many2Many Input Tags, Odoo Widget many2many_tags, Base Field Many2Many Link Tags, 2Many Link Tags, Base Field Many2Many Tags Link, 2Many Tags Link',
+    'description': "New style for Many2Many Tags Link Widget",
+    'author': 'NEWAY Solutions',
+    'maintainer': 'NEWAY Solutions',
+    'price': '0.0',
+    'currency': 'USD',
+    'website': 'https://neway-solutions.com',
+    'license': 'LGPL-3',
+    'images': [
+        'static/description/icon.png'    
+    ],
+    'depends': [
+        
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
