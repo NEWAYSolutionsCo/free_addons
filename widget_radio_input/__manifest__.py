cfea3762a190306cea1f04df5c25dc96886d0236
NEWAYSolutionsCo <newaysolutions.co@gmail.com>
2022-10-04 13:25:16 +0100

    Add Radio Widget

---
+++ widget_radio_input/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Radio Input Widget',
+    'version': '14.0.1.0.0',
+    'sequence': 1,
+    'summary': 'Radio Input Widget, Web Widget Radio Input, Web Widget Input Radio, Input Design, New Design Input, Web Responsive Input, Web Backend Input, Web Field Widget, Odoo Widget Input Radio',
+    'description': "Attractive Radio Input to use.",
+    'author': 'NEWAY Solutions',
+    'maintainer': 'NEWAY Solutions',
+    'price': '0.0',
+    'currency': 'USD',
+    'website': 'https://neway-solutions.com',
+    'license': 'LGPL-3',
+    'images': [
+        'static/description/icon.png'
+    ],
+    'depends': [
+        
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+        
+    ],
+}
