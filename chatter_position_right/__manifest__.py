0eca1a0fb9b85859cefa4c916cf775eec9e492d5
NEWAYSolutionsCo <newaysolutions.co@gmail.com>
2022-10-14 11:32:13 +0100

    Update Key Search

--- chatter_position_right/__manifest__.py
+++ chatter_position_right/__manifest__.py
@@ -5,7 +5,17 @@
     'name': 'Chatter Position Right',
     'version': '13.0.1.0.0',
     'sequence': 1,
-    'summary': 'Odoo Chatter in Position Right, All in one Chatter, All in one Odoo Chatter, Odoo Chatter Message, Mail Activity Chatter, Right Odoo Chatter, Right Chatter Log, Right Chatter Notes, Web Backend Chatter, Web Responsive Chatter, Web Chatter Sidebar, Web Sidebar Chatter',
+    'summary': """
+        Odoo Chatter in Position Right, All in one Chatter, All in one Odoo Chatter, Odoo Chatter Message, Mail Activity Chatter, Edit Notes, 
+        Right Odoo Chatter, Right Chatter Log, Right Chatter Notes, Web Backend Chatter, Web Chatter Sidebar, Chatter Note Visibility Note, 
+        Web Sidebar Chatter, Mail Messages Chatter History Chatter Messages, Chatter Activities Visibility Activities, Web Responsive Chatter,
+        Inbox Chatter Inbox Mail Activity, Send Messages, Schedule Activity Schedule Activities Scheduler, Chatter Message Visibility Message,
+        Chatter Messages Visibility Messages, Chatter Activity Visibility Activity, Chatter Notes Visibility Notes, Edit Messages, Log a note,
+        Mail Activities Scheduler, Live Chat, Chatter Room, ChatRoom, Configurable Chatter Position, Chatter Attachement, User Chatter Design,
+        Configure Chatter Position Chatter Customize Position Chatter Customization, Show Chatter Show, Toggle Chatter Toggle User Chatter, 
+        Mail Chatter Email History Chatter Mail Show History, Company Chatter Comments, Chatter User Activities, Disable Chatter, Chatter Panel, 
+        Chatter User Mails, Chatter User Activity, Chatter User Notes, Chatter Sidebar, Hide Chatter Hide, Edit Logs, Log notes, Activity Preview
+    """,
     'description': "Change Odoo Chatter Position from bottom to right.",
     'author': 'NEWAY Solutions',
     'maintainer': 'NEWAY Solutions',
