ef818b9244a9e83a2daf17136f205652a35dbf39
NEWAYSolutionsCo <newaysolutions.co@gmail.com>
2022-10-18 11:12:38 +0100

    Upload Toggle Switch

---
+++ widget_toggle_switch/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Toggle Switch Widget',
+    'version': '14.0.1.0.0',
+    'sequence': 1,
+    'summary': """
+        Toggle Switch Input Widget, Web Widget Toggle Switch, Web Widget Input Toggle Switch, Input Design, New Design Input Switch Toogle,
+        Checkbox Input Widget, Web Widget Checkbox Input, Web Widget Input Checkbox, Input Design, New Design Input Widget, 
+        Web Responsive Input, Web Backend Input, Web Field Widget, Odoo Widget Input Checkbox, Checked Input Checked Widget,
+        Input Style New Style Input Design Checkbox Style Checkbox, Bootstrap Input Odoo Widget Odoo Input Form View Input,
+        Bootsrap Kits Forms, Bootstrap Forms Kits Inputs Widget, Web Field Odoo Checkbox, Odoo Field Widget Field Checkbox,
+        Checkbox Field Boolean Field Style, Selected Checkbox, Design Form Style, Boolean Checkbox Boolean Input Field,
+        Switch Input Widget, Web Widget Switch Input, Web Widget Input Switch, Input Design, New Design Input Widget, 
+        Web Responsive Input, Web Backend Input, Web Field Widget, Odoo Widget Input Switch, Checked Input Checked Widget,
+        Input Style New Style Input Design Switch Style Switch, Bootstrap Input Odoo Widget Odoo Input Form View Input,
+        Bootsrap Kits Forms, Bootstrap Forms Kits Inputs Widget, Web Field Odoo Switch, Odoo Field Widget Field Switch,
+        Switch Field Boolean Field Style, Selected Switch, Design Form Style, Boolean Switch Boolean Input Field 
+        Web Responsive Input, Web Backend Input, Web Field Widget, Odoo Widget Input Toggle Switch,
+    """,
+    'description': "Attractive Toggle Switch Input to use.",
+    'author': 'NEWAY Solutions',
+    'maintainer': 'NEWAY Solutions',
+    'price': '0.0',
+    'currency': 'USD',
+    'website': 'https://neway-solutions.com',
+    'license': 'LGPL-3',
+    'images': [
+        'static/description/icon.png'
+    ],
+    'depends': [
+        
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+        
+    ],
+}
