db6f54479cd3fab1a8ff25fe9840e8b35333e502
NEWAYSolutionsCo <newaysolutions.co@gmail.com>
2022-11-06 20:29:05 +0100

    Upload Widget Checkbox Input

---
+++ widget_checkbox_input/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Checkbox Input Widget',
+    'version': '16.0.1.0.0',
+    'sequence': 1,
+    'summary': """
+        Checkbox Input Widget, Web Widget Checkbox Input, Web Widget Input Checkbox, Input Design, New Design Input Widget, 
+        Web Responsive Input, Web Backend Input, Web Field Widget, Odoo Widget Input Checkbox, Checked Input Checked Widget,
+        Input Style New Style Input Design Checkbox Style Checkbox, Bootstrap Input Odoo Widget Odoo Input Form View Input,
+        Bootsrap Kits Forms, Bootstrap Forms Kits Inputs Widget, Web Field Odoo Checkbox, Odoo Field Widget Field Checkbox,
+        Checkbox Field Boolean Field Style, Selected Checkbox, Design Form Style, Boolean Checkbox Boolean Input Field
+    """,
+    'description': "Attractive Checkbox Input to use.",
+    'author': 'NEWAY Solutions',
+    'maintainer': 'NEWAY Solutions',
+    'price': '0.0',
+    'currency': 'USD',
+    'website': 'https://neway-solutions.com',
+    'license': 'LGPL-3',
+    'images': [
+        'static/description/icon.png'
+    ],
+    'depends': [
+        
+    ],
+    'data': [
+        
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'widget_checkbox_input/static/src/scss/input.scss',
+        ],
+    },
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+        
+    ],
+}
